59fe2ed3f2d29e8692d69006e5c92d4aa88d60e5
Harbdrain <mail@d-demchenko.ru>
2023-05-17 13:41:29 +0300

    Made Utils.canner final.

--- src/main/java/com/danil/tictactoe/Utils.java
+++ src/main/java/com/danil/tictactoe/Utils.java
@@ -3,5 +3,5 @@ package com.danil.tictactoe;
 import java.util.Scanner;
 
 public class Utils {
-    public static Scanner scanner = new Scanner(System.in);
+    public final static Scanner scanner = new Scanner(System.in);
 }
